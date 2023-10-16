90d547a618b58da7f4e91e84e6b90c42375a279d
Emmanuel Adovor <adovoremmanuel8@gmail.com>
2023-10-16 23:47:41 +0000

    added addNumbers() method

--- Hello.java
+++ Hello.java
@@ -1,6 +1,11 @@
 public class Hello{
+  public int addNumbers(int value1, int value2){
+    return value1 + value2;
+  }
   public static void main(Strin[] args){
+
     System.out.println("Hello, my team members! How are you all doing today");
+
   }
   
 }
